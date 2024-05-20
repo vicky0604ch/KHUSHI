af1f1d895f46b9701331f6bb5a007a6aa574e52b
Rajputshivsingh65 <166673972+Rajputshivsingh65@users.noreply.github.com>
2024-05-20 23:02:11 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -113,11 +113,11 @@ async def ping_com(client, message: Message, _):
                 [
                     InlineKeyboardButton(
                         text="✦ ɢʀᴏᴜᴘ ✦",
-                        url=f"https://t.me/TG_FRIENDSS",
+                        url=f"https://t.me/Dns_Official_Channel",
                     ),
                     InlineKeyboardButton(
                         text="✧ ᴍᴏʀᴇ ✧",
-                        url=f"https://t.me/VIP_CREATORS",
+                        url=f"https://t.me/Dns_Official_Channel",
                     ),
                 ],
                 [
